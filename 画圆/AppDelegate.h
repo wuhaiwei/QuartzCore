97925cc54fb39f9131edf1ba91a934f249fb4078
wuhaiwei <wuhaiwei0809@163.com>
2016-09-09 17:45:14 +0800

    Initial Commit

---
+++ 画圆/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  画圆
+//
+//  Created by  wuhiwi on 16/9/9.
+//  Copyright © 2016年 wanglibank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 画圆/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  画圆
+//
+//  Created by  wuhiwi on 16/9/9.
+//  Copyright © 2016年 wanglibank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
