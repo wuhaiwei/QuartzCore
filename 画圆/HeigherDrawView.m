678531e0a7dc36ffa4106678a51e5f781242c884
wuhaiwei <wuhaiwei0809@163.com>
2016-09-18 17:40:01 +0800

    ..

--- 画圆/HeigherDrawView.m
+++ 画圆/HeigherDrawView.m
@@ -20,6 +20,18 @@
 }
 - (void)drawRect:(CGRect)rect
 {
+    //第一种绘图形式：在UIView的子类方法drawRect：中绘制一个蓝色圆，使用UIKit在Cocoa为我们提供的当前上下文中完成绘图任务。
+    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, 100, 100)];
+    bezierPath.lineWidth = 0.5;
+    [[UIColor blueColor] setStroke];
+    [bezierPath stroke];
     
+    
+    //第二种绘图形式：使用Core Graphics实现绘制蓝色圆。
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetLineWidth(context, 0.5);
+    CGContextAddEllipseInRect(context, CGRectMake(120, 0, 100, 100));
+    CGContextDrawPath(context, kCGPathStroke);
 }
+
 @end
